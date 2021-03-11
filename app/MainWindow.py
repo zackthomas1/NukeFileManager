48d6632f5f7e3e20e5474d315e288623dc125f18
zack thomas <zackthomas143@outlook.com>
2021-03-10 16:00:57 -0800

    Removed doubleClick_script method in MainWindow and combined functionality with calling_launch_nukeindie

--- app/MainWindow.py
+++ app/MainWindow.py
@@ -40,38 +40,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Slot-Signal connections 
         # -----------------------
 
-        self.nkFiles_listView.doubleClicked.connect(self.doubleClick_script)
+        self.nkFiles_listView.doubleClicked.connect(self.calling_launch_nukeindie)
 
         self.rootDir_lineEdit.editingFinished.connect(self.entered_root_dir)
         self.showCode_comboBox.currentIndexChanged.connect(self.selected_show_code)
         self.shotCode_comboBox.currentIndexChanged.connect(self.selected_shot_code)
 
-        self.enter_shotinfo_pushButton.pressed.connect(self.pressed_update_shotList)
+        self.enter_shotinfo_pushButton.pressed.connect(self.calling_update_shotList)
 
-        self.launchNukeIndie_pushButton.pressed.connect(self.pressed_launch_nukeindie)
-
-    def doubleClick_script(self): 
-
-        # Gets Indexes (list of a single item, since nkFiles_listView is in single select mode)
-        indexes = self.nkFiles_listView.selectedIndexes()
-
-        if indexes: 
-            # Indexes is a list of a single item in single-select mode
-            index = indexes[0]
-            scriptName = self.scriptsViewModel.scripts[index.row()]
-            logging.debug("MainWindow::doubleClick_script-> " +
-                        "from FileManger calling doubleClick_launch_script_nukeIndie() method " +
-                        "with parameter: " + scriptName)
-            self.fileMan.doubleClick_launch_script_nukeIndie(scriptName)
-        else: 
-            logging.error("ERROR: MainWindow::doubleClick_script-> no index selected")
-            raise Exception
+        self.launchNukeIndie_pushButton.pressed.connect(self.calling_launch_nukeindie)
 
     def entered_root_dir(self): 
         inputRootDir = self.rootDir_lineEdit.text()
         logging.debug("MainWindow::enter_root_dir -> " + 
                         "from FileManger calling set_path_baseName method " + 
-                        "with parameter: " + inputRootDir)
+                        "with parameter: %s" % inputRootDir)
 
         self.fileMan.set_root_dir(inputRootDir)
 
@@ -82,7 +65,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.showCodeModel.layoutChanged.emit()
         logging.debug("MainWindow::enter_root_dir-> Updating showCode_comboBox with: %s" % showList)
 
-
     def selected_show_code(self):
 
         # Retrieve string of selected comboBox index
@@ -91,7 +73,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         logging.debug("MainWindow::enter_show_code -> " + 
                         "from FileManger calling set_show_code method " + 
-                        "with parameter: " + inputShowCode)
+                        "with parameter: %s" % inputShowCode)
 
         self.fileMan.set_show_code(inputShowCode)
 
@@ -113,27 +95,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                         "with parameter: %s" % str(inputShotCode))
         self.fileMan.set_shot_code(inputShotCode)
 
-    def pressed_update_shotList(self): 
-        logging.debug("MainWindow::pressed_update_shotList -> " + 
+    def calling_update_shotList(self): 
+        logging.debug("MainWindow::calling_update_shotList -> " + 
                         "from FileManger calling update_shotList() method ")
 
         self.scriptsViewModel.scripts = self.fileMan.update_shotList()
         self.scriptsViewModel.layoutChanged.emit()
-        logging.debug("MainWindow::pressed_update_shotList-> scriptsViewModel Updated")
+        logging.debug("MainWindow::calling_update_shotList-> scriptsViewModel Updated")
 
-    def pressed_launch_nukeindie(self): 
+    def calling_launch_nukeindie(self): 
         
+        # Gets Indexes (list of a single item, since nkFiles_listView is in single select mode)
         indexes = self.nkFiles_listView.selectedIndexes() 
 
         # If script is selected
         if indexes: 
             index = indexes[0]
             scriptName = self.scriptsViewModel.scripts[index.row()]
+            logging.debug("MainWindow::calling_launch_nukeindie-> " +
+                            "from FileManger executing launch_nukeindie method " + 
+                            "with parameter %s" % scriptName)
             self.fileMan.launch_nukeindie(scriptName)
-            logging.debug("MainWindow::launch_nukeindie-> " +
-                            "from FileManger calling launch_nukeindie method" + 
-                            "with parameter")
         else:
+            logging.debug("MainWindow:: calling_launch_nukeindie-> " +
+                            "from FileManger executing launch_nukeindie method")
             self.fileMan.launch_nukeindie()
-            logging.debug("MainWindow::launch_nukeindie-> " +
-                            "from FileManger calling launch_nukeindie method")
